e582ffd6f751d2b572ac31415c3c264c3d85f76b
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2026-03-19 13:55:21 +0100

    primer

---
+++ Primer/src/HolaMundoWeb.java
@@ -0,0 +1,9 @@
+
+public class HolaMundoWeb {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo Web");
+		System.out.println("Aprenderemos Java para el desarrollo web");
+	}
+}
